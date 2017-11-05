765c7933b8174a06955706a0c80f09fc6c0e86a6
Youssef Rizkalla <32617102+JohnWinter13@users.noreply.github.com>
2017-11-05 14:40:41 -0500

    Changes to C++ DFS
    Formatting and commenting

--- C++/Depth First Search.cpp
+++ C++/Depth First Search.cpp
@@ -1,36 +1,37 @@
-#include "stdafx.h"
 #include <vector>
 
 const int maxN = 10;
 std::vector<int> adjList[maxN];
 bool visited[maxN];
+
+//A DFS implementation with an adjacency list to check if a path exists between a source and a sink
 bool DFS(int vertex, int sink)
 {
-	visited[vertex] = true;
-	for (int neighbour : adjList[vertex])
-		if (!visited[neighbour])
-			DFS(neighbour, sink);
+    visited[vertex] = true;
+    for (int neighbour : adjList[vertex])
+        if (!visited[neighbour])
+            DFS(neighbour, sink);
 
-	return visited[sink];
+    return visited[sink];
 }
 
 #include <iostream>
 int main()
 {
-	//A bidirectional graph
-	adjList[2].push_back(1);
-	adjList[1].push_back(2);
-	adjList[5].push_back(2);
-	adjList[2].push_back(5);
-	adjList[6].push_back(7);
-	adjList[7].push_back(6);
+    //A bidirectional graph for testing
+    adjList[2].push_back(1);
+    adjList[1].push_back(2);
+    adjList[5].push_back(2);
+    adjList[2].push_back(5);
+    adjList[6].push_back(7);
+    adjList[7].push_back(6);
 
-	std::cout << DFS(2, 5) << "\n";
-	std::memset(visited, false, sizeof visited);
-	std::cout << DFS(1, 5) << "\n";
-	std::memset(visited, false, sizeof visited);
-	std::cout << DFS(1, 7) << "\n";
-	std::memset(visited, false, sizeof visited);
-	std::cout << DFS(6, 7) << "\n";
+    std::cout << DFS(2, 5) << "\n";
+    std::memset(visited, false, sizeof visited);
+    std::cout << DFS(1, 5) << "\n";
+    std::memset(visited, false, sizeof visited);
+    std::cout << DFS(1, 7) << "\n";
+    std::memset(visited, false, sizeof visited);
+    std::cout << DFS(6, 7) << "\n";
     return 0;
 }
