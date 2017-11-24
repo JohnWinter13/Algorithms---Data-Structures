7ea58cabe6f926b3b9adaae1a06328f756b2646c
Youssef Rizkalla <32617102+JohnWinter13@users.noreply.github.com>
2017-11-23 22:40:58 -0500

    Reformatted DisjointSet.cs

--- C#/Disjoint Set.cs
+++ C#/Disjoint Set.cs
@@ -1,29 +1,29 @@
-    class DisjointSet
+public class DisjointSet
+{
+    public readonly int[] DS;
+
+    public DisjointSet(int capacity)
     {
-        public readonly int[] DS;
-
-        public DisjointSet(int capacity)
-        {
-            DS = new int[capacity];
-            for (int i = 0; i < capacity; i++)
-                DS[i] = i;
-        }
-
-        public int Root(int i)
-        {
-            if (DS[i] != i)
-                DS[i] = Root(DS[i]);
-            return DS[i];
-        }
-
-        public bool Find(int i, int j)
-        {
-            return Root(i) == Root(j);
-        }
-
-        public void Unite(int i, int j)
-        {
-            DS[Root(i)] = Root(j);
-        }
+        DS = new int[capacity];
+        for (int i = 0; i < capacity; i++)
+            DS[i] = i;
     }
-    
+
+    public int Root(int i)
+    {
+        if (DS[i] != i)
+            DS[i] = Root(DS[i]);
+        return DS[i];
+    }
+
+    public bool Find(int i, int j)
+    {
+        return Root(i) == Root(j);
+    }
+
+    public void Unite(int i, int j)
+    {
+        DS[Root(i)] = Root(j);
+    }
+}
+
