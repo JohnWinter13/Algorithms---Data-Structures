1d31a102b3980bca3a72391a141e5fe883bee0ef
Youssef Rizkalla <32617102+JohnWinter13@users.noreply.github.com>
2017-12-31 19:30:17 -0500

    Reformatted library and added one solution
    Now has a folder for contest solutions and a folder for standard algorthims / ds code. Also added a solution for UTS '15 P3.

---
+++ Algorithms-Data-Structures/C#/Shortest Path Faster Algorithm.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+
+public class SPFA<T>
+{
+    public Dictionary<T, int> ShortestPaths(WeightedGraph<T> Graph, T Source)
+    {
+        var Dists = new Dictionary<T, int>();
+        foreach (var Vertex in Graph.AdjacencyList)
+            Dists[Vertex.Key] = 1 << 30;
+        var Q = new Queue<T>();
+        Q.Enqueue(Source);
+        Dists[Source] = 0;
+
+        while (Q.Count > 0)
+        {
+            var Vertex = Q.Dequeue();
+            foreach (var Neighbour in Graph.AdjacencyList[Vertex])
+            {
+                int TempDist = Dists[Vertex] + Neighbour.Item2;
+                if (TempDist < Dists[Neighbour.Item1])
+                {
+                    Dists[Neighbour.Item1] = TempDist;
+                    Q.Enqueue(Neighbour.Item1);
+                }
+            }
+        }
+        return Dists;
+    }
+}
+
---
+++ Algorithms-Data-Structures/C#/Weighted Graph.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class WeightedGraph<T>
+{
+    public Dictionary<T, HashSet<Tuple<T, int>>> AdjacencyList { get; } = new Dictionary<T, HashSet<Tuple<T, int>>>();
+    public int VertexCount { get; private set; } = 0;
+
+    public void AddVertex(T Vertex)
+    {
+        AdjacencyList[Vertex] = new HashSet<Tuple<T, int>>();
+        VertexCount++;
+    }
+
+    public void AddDirectedEdge(T A, T B, int W)
+    {
+        AdjacencyList[A].Add(Tuple.Create(B, W));
+    }
+
+    public void AddBiEdge(T A, T B, int W)
+    {
+        AdjacencyList[A].Add(Tuple.Create(B, W));
+        AdjacencyList[B].Add(Tuple.Create(A, W));
+    }
+
+    public void RemoveDirectedEdge(T A, T B, int W)
+    {
+        AdjacencyList[A].Remove(Tuple.Create(B, W));
+    }
+
+    public void RemoveBiEdge(T A, T B, int W)
+    {
+        AdjacencyList[A].Remove(Tuple.Create(B, W));
+        AdjacencyList[B].Remove(Tuple.Create(A, W));
+    }
+}
+
